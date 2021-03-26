75708e3d35fdee4dead9a9357db6fa69308b72ac
jocholiveira <35350899+jocholiveira@users.noreply.github.com>
2021-03-26 22:01:54 +0000

    start of requestOWM function

--- meteo2maps.py
+++ meteo2maps.py
@@ -4,6 +4,8 @@ from datetime import datetime
 from psycopg2 import extras as psy2extras
 from geo.Geoserver import Geoserver
 
+# camelCase style adopted
+
 # To disable geopandas CRS warnings in terminal
 warnings.filterwarnings('ignore')
 
@@ -12,21 +14,22 @@ basePath = 'C:\\saprog\\projeto'
 outputPath = 'C:\\saprog\\projeto\\output\\'
 os.chdir(basePath)
 
-def deleteAndCreateOutput():
-    print ('A limpar e criar directoria de outputs...')
-    outDir = os.path.join(basePath, 'output')
-    if os.path.isdir(outDir):
-        shutil.rmtree(outDir)
-    os.mkdir(outDir)
 
-# deleteAndCreateOutput()
+print('\n1 - Working on initial steps... Please, wait a moment.')
 
+def outputDir():
+    if os.path.exists(outputPath) == False:
+        os.mkdir(basePath, 'output')
+        print('\n2 - Output dir. created.')
+    else:
+        print('\n2 - Found output dir.')
 
-print('Working on initial steps... Please, wait a moment.')
+outputDir()
+
 
 # Working on districts
 if os.path.exists(outputPath+'districts.shp') == False:
-    print('districts.shp not found. Let\'s work on it.')
+    print('\n3 - districts.shp not found. Let\'s work on it.')
     # Reading main CAOP shapefile (it needs to already exist)
     caop = geopandas.read_file('caop.shp', encoding='utf-8')
     # Executing dissolve from parishes to districts
@@ -36,7 +39,7 @@ if os.path.exists(outputPath+'districts.shp') == False:
     districts = geopandas.read_file(outputPath+'districts.shp', encoding='utf-8')
     # # districts = districts.to_crs(3763)
 else:
-    print('Found districts.shp')
+    print('\n3 - Found districts.shp')
     # Reading districts shapefile that already exists
     districts = geopandas.read_file(outputPath+'districts.shp', encoding='utf-8')
     # # districts = districts.to_crs(3763)
@@ -57,7 +60,7 @@ def getCoordTogether(geoDataFrame):
     return coordDic
 
 coord = getCoordTogether(districts)
-print('District coordinates compiled.')
+print('\n4 - District coordinates compiled.')
 
 # print(coord)
 
@@ -90,7 +93,24 @@ if checkPgTable(conn, 'districts') == False:
           "-lco", "GEOMETRY_NAME=the_geom", "-lco", "FID=gid", "-lco",
           "PRECISION=no", "-nlt", "PROMOTE_TO_MULTI", "-nln", "districts", "-overwrite"]
     subprocess.check_call(command)
-    print('Districts table loaded into meteo database.')
+    print('\n5 - Districts table loaded into meteo database.')
 else:
-    print('Districts table already exists in meteo database')
+    print('\n5 - Districts table already exists in meteo database.')
 
+
+# Meteo request to API
+def requestOWM():
+    '''
+    Função para recolha dos dados meteorológicos provenientes da OpenWeather One Call API.
+    Exige ao utilizador a introduçaõ interativa do tipo de pedido de mapa meteorológico.
+    Devolve uma dataframe (pandas) das váriáveis meteorológicas por distrito para o momento indicado.
+    '''
+    requestType = input('\n6 - Specify the wanted type of meteomap request [Y for yesterday, N for now, T for tomorrow]:' )
+    if requestType == 'Y':
+        
+    elif requestType == 'N':
+
+    elif requestType == 'T':
+        print('bela shit')
+
+requestOWM()
